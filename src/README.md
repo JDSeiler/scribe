88be16ab0db20388f83777155b15286b097830ea
JDSeiler <jseiler@radford.edu>
2022-03-03 14:57:13 -0500

    Finish script and add README

---
+++ src/README.md
@@ -0,0 +1,37 @@
+# Scribe
+Scribe is a command line utility for converting the binary data files of the MNIST data set (see: http://yann.lecun.com/exdb/mnist/)
+to a collection of BMP images.
+## Running the script
+To compile, run `cargo build --release`. You will find the executable in the directory `./target/release`. 
+
+The script requires 4 parameters:
+- Two paths relative to the current working directory
+  - The location of the image data file
+  - The location of the corresponding labels file
+- A 1-based index describing which image to start with
+- How many images to read
+
+For example, if your directory structure looks like this:
+```
+./mnist
+  t10k-images-idx3-ubyte
+  t10k-labels-idx1-ubyte
+  train-images-idx3-ubyte
+  train-labels-idx1-ubyte
+```
+You could invoke the script like this to dump all 10,000 testing images:
+`scribe ./mnist/t10k-images-idx3-ubyte ./mnist/t10k-labels-idx1-ubyte 1 10000`
+
+As another example, you could just dump 3,000 images, starting from image number 6,000:
+`scribe ./mnist/t10k-images-idx3-ubyte ./mnist/t10k-labels-idx1-ubyte 6000 3000`
+
+The script will put all output images in a directory called `out`. This directory will
+be created in the current working directory. The images are named in the form:
+`img-{image_index}-is-a-{image_label}.bmp`. So: `img-50-is-a-9.bmp` is the 50th image
+read by the script (not the 50th image overall!) and it's a 9.
+
+You can also print a help message by invoking the script with a single option: `--help`, like so:
+`scribe --help`.
+
+The tool does not have very robust error reporting and is not configurable aside from the options
+described here, but it is functional!
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,129 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read, Seek, SeekFrom};
 use std::process::exit;
+use crate::FailureReason::{FileIoFailure, FileTypeMismatch, InvalidRange};
+use image::{GrayImage, ImageFormat};
+
+const IMAGE_FILE: u32 = 2051;
+const LABEL_FILE: u32 = 2049;
+const BYTES_PER_IMAGE: u32 = 28 * 28;
+
+type ImageData = [u8; BYTES_PER_IMAGE as usize];
+
+#[derive(Debug)]
+enum FailureReason {
+    FileIoFailure,
+    FileTypeMismatch,
+    InvalidRange
+}
+
+type ImageFileWithLabels = (BufReader<File>, BufReader<File>);
 
 fn main() {
     let args = read_args();
-    println!("{:#?}", args);
+
+    if let Some(args) = args {
+        let file_handles = prepare_read_handles(&args).expect("Eek! Something went badly wrong!");
+        let res = dump_images(file_handles, args.quantity);
+        println!("Operation result: {:#?}", res);
+    }
+}
+
+fn dump_images(file_handles: ImageFileWithLabels, quantity: u32) -> std::io::Result<()> {
+    let (mut source, mut labels) = file_handles;
+    let mut num_images_read = 0;
+    while num_images_read < quantity {
+        let label = read_u8(&mut labels)?;
+        let image_data = read_image_data(&mut source)?;
+
+        let img = GrayImage::from_raw(28, 28, image_data);
+        match img {
+            Some(buf) => {
+                let img_destination = format!("./out/img-{}-is-a-{}.bmp", num_images_read, label);
+                buf.save_with_format(img_destination, ImageFormat::Bmp).expect("Failed to save image");
+            },
+            None => {
+                eprintln!("Failed to create image buffer from data!")
+            }
+        }
+        num_images_read += 1;
+    }
+
+    Ok(())
+}
+
+fn prepare_read_handles(args: &Args) -> std::io::Result<ImageFileWithLabels> {
+    let mut source = get_read_handle(args.input_source.as_str())?;
+    let mut labels = get_read_handle(args.input_labels.as_str())?;
+
+    operation_is_valid(&mut source, IMAGE_FILE, args).unwrap_or_else(|failure_reason| {
+        println!("Operation invalid due to issue with images file: {:#?}", failure_reason);
+        exit(1);
+    });
+    operation_is_valid(&mut labels, LABEL_FILE, args).unwrap_or_else(|failure_reason| {
+        println!("Operation invalid due to issue with labels file: {:#?}", failure_reason);
+        exit(1);
+    });
+
+    source.seek(SeekFrom::Start(16))?;
+    labels.seek(SeekFrom::Start(8))?;
+
+    let source_read_offset = (args.start_from - 1) * BYTES_PER_IMAGE;
+    let label_read_offset = args.start_from - 1;
+
+    source.seek_relative(source_read_offset as i64)?;
+    labels.seek_relative(label_read_offset as i64)?;
+
+    Ok((source, labels))
+}
+
+fn get_read_handle(path: &str) -> std::io::Result<BufReader<File>> {
+    let f = File::open(path)?;
+    Ok(BufReader::new(f))
+}
+
+fn operation_is_valid(file: &mut BufReader<File>, expected_magic_number: u32, args: &Args) -> std::result::Result<(), FailureReason> {
+    let magic_number = match read_u32(file) {
+        Ok(mn) => mn,
+        Err(_) => { return Err(FileIoFailure); }
+    };
+    let num_images = match read_u32(file) {
+        Ok(num) => num,
+        Err(_) => { return Err(FileIoFailure); }
+    };
+
+    if magic_number != expected_magic_number {
+        Err(FileTypeMismatch)
+    } else {
+        match (args.quantity >= 1) && (args.start_from >= 1) && (args.start_from + args.quantity - 1) <= num_images {
+            false => Err(InvalidRange),
+            true => Ok(())
+        }
+    }
+}
+
+fn read_u32(reader: &mut BufReader<File>) -> std::io::Result<u32> {
+   let mut u32_bytes: [u8; 4] = [0; 4];
+    reader.read_exact(&mut u32_bytes)?;
+
+    Ok(u32::from_be_bytes(u32_bytes))
+}
+
+fn read_u8(reader: &mut BufReader<File>) -> std::io::Result<u8> {
+    let mut u8_byte: [u8; 1] = [0; 1];
+    reader.read_exact(&mut u8_byte)?;
+
+    Ok(u8_byte[0])
+}
+
+fn read_image_data(reader: &mut BufReader<File>) -> std::io::Result<Vec<u8>> {
+    let mut image_bytes: ImageData = [0; BYTES_PER_IMAGE as usize];
+    reader.read_exact(&mut image_bytes)?;
+
+    // MNIST data has 0 as white and 255 as black for some reason.
+    // Exactly swapped from "normal"
+    Ok(Vec::from(image_bytes).into_iter().map(|b| 255 - b).collect())
 }
 
 fn read_args() -> Option<Args> {
@@ -31,8 +151,8 @@ fn read_args() -> Option<Args> {
     } else {
        match &raw_args[..] {
            [source, label, raw_start, raw_quantity] => {
-               let start_from: u64 = raw_start.parse::<u64>().expect("Could not parse argument start_from to a u64");
-               let quantity: u64 = raw_quantity.parse::<u64>().expect("Could not parse argument quantity to a u64");
+               let start_from: u32 = raw_start.parse::<u32>().expect("Could not parse argument start_from to a u64");
+               let quantity: u32 = raw_quantity.parse::<u32>().expect("Could not parse argument quantity to a u64");
                return Some(Args {
                    input_source: source.to_owned(),
                    input_labels: label.to_owned(),
@@ -64,6 +184,6 @@ fn print_help_and_exit() {
 struct Args {
     input_source: String,
     input_labels: String,
-    start_from: u64,
-    quantity: u64
+    start_from: u32,
+    quantity: u32
 }
